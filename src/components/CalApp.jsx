ac63b8ae08a20be33d20b299f796f839e0d4479b
naseer4864 <naseernoor4864@gmail.com>
2023-04-14 00:36:49 +0100

    first commit

--- src/components/CalApp.jsx
+++ src/components/CalApp.jsx
@@ -2,10 +2,17 @@ import { useState } from "react";
 import KeyPads from "./keypad";
 
 const CalApp = () => {
-    const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
+  const [history, setHistory] = useState(
+    JSON.parse(localStorage.getItem("history")) || []
+  );
 
   const handleNumberClick = (value) => {
     setInput(input + value);
+  };
+
+  const handleOperatorClick = (operator) => {
+    setInput(input + operator);
   };
 
   const handleClearClick = () => {
@@ -13,17 +20,53 @@ const CalApp = () => {
   };
 
   const handleEqualsClick = () => {
-    setInput(eval(input).toString());
+    try {
+      const result = eval(input);
+      setInput(result.toString());
+
+      setHistory([...history, { input, result }]);
+      localStorage.setItem(
+        "history",
+        JSON.stringify([...history, { input, result }])
+      );
+    } catch (error) {
+      setInput("Error");
+    }
   };
-    return ( 
-        <div className="calculator">
-            <input type="text" value={input} readOnly />
-            <KeyPads
-            handleClearClick={handleClearClick}
-            handleNumberClick={handleNumberClick}
-            handleEqualsClick={handleEqualsClick} />
-        </div>
-     );
-}
- 
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleClearHistoryClick = () => {
+    localStorage.removeItem("history");
+    setHistory([]);
+  };
+
+  return (
+    <div className="calculator">
+      <input type="text" value={input} onChange={handleChange} />
+      <KeyPads
+        handleClearClick={handleClearClick}
+        handleNumberClick={handleNumberClick}
+        handleOperatorClick={handleOperatorClick}
+        handleEqualsClick={handleEqualsClick}
+      />
+      {history.length > 0 && (
+        <button
+          onClick={handleClearHistoryClick}
+          style={{ backgroundColor: "brown", fontSize: "1rem", margin: "10px" }}
+        >
+          Clear History
+        </button>
+      )}
+      {history.map((calculation, index) => (
+        <li key={index}>
+          {calculation.input} = {calculation.result}
+        </li>
+      ))}
+    </div>
+  );
+};
+
 export default CalApp;
